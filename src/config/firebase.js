70b75486ca3f63eb5f938fe7e9c32a2a350e1439
wisnuekas <wisnueka11@gmail.com>
2020-10-11 21:05:57 +0700

    update cfg

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const config = firebase.initializeApp({
-  apiKey: "AIzaSyAFvOqQXybDYZsIkYmsYA08wegfkDHv6tE",
-  authDomain: "i-life-833fa.firebaseapp.com",
-  databaseURL: "https://i-life-833fa.firebaseio.com",
-  projectId: "i-life-833fa",
-  storageBucket: "i-life-833fa.appspot.com",
-  messagingSenderId: "551845352260",
-  appId: "1:551845352260:web:055b23ba46ef016f23fd11",
+  apiKey: "aaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+  authDomain: "aaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+  databaseURL: "aaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+  projectId: "aaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+  storageBucket: "aaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+  messagingSenderId: "aaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+  appId: "aaaaaaaaaaaaaaaaaaaaaaaaaaaa",
 });
 
 export default config;
